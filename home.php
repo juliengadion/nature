96c34aba2ac331ef741b5e48b38bd9c40ffdb656
juliengadion <legadijoncte@hotmail.fr>
2019-09-05 11:47:29 +0200

    42commit

--- home.php
+++ home.php
@@ -12,21 +12,17 @@ get_header();
         <div class="bandeau">
             <div class="row">
                 <div class="col-lg-6">
-                    <img src="http://marion2/wp-content/uploads/2019/09/panneau-bois-suspendu-clou-rouille.png" alt="panneau">
+                    <img id="panneau"
+                        src="http://marion2/wp-content/uploads/2019/09/panneau-bois-suspendu-clou-rouille-copie-1.png"
+                        alt="panneau">
                 </div>
 
-                <div class="col-lg-6">
-                    <p>Après la naissance de ma première fille, je me suis tournée vers le naturel au quotidien. Il
-                        était
-                        primordial pour moi d’utiliser pour elle uniquement des produits naturels.
-                        Le naturel c’est alors imposé partout : alimentation, santé, bien être, …
-
-                        Pour en arriver là je me suis énormément documenté avec toujours l’envie d’en savoir plus. C’est
-                        la
-                        que la naturopathie est devenue une évidence pour moi.
-                    </p>
+                <div class="col-lg-6" id="citation">
+              <p>« Tout ce dont nous avons besoin pour rester sain,
+                    la nature nous l’a donné en abondance »</p>
+                 <p>Sébastian Kneipp</p> 
                 </div>
-               
+
             </div>
         </div>
         <!-- bandeau fin -->
@@ -37,8 +33,8 @@ get_header();
 
                 <div class="col-lg-4">
                     <div class="card" id="card1">
-                        <img src="http://marion2/wp-content/uploads/2019/09/pexels-photo-129743.jpg" class="card-img-top" id="cardimg1"
-                            alt="...">
+                        <img src="http://marion2/wp-content/uploads/2019/09/pexels-photo-459301.jpeg"
+                            class="card-img-top" id="cardimg1" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">Bilan d'hygiène de vie</h5>
                             <p class="card-text">This is a longer card with supporting text below as a natural lead-in
@@ -51,7 +47,7 @@ get_header();
                 <div class="col-lg-4">
                     <div class="card" id="card2">
                         <img src="http://marion2/wp-content/uploads/2019/09/pexels-photo-257840.jpg"
-                            class="card-img-top"  id="cardimg2" alt="...">
+                            class="card-img-top" id="cardimg2" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">Conseils en alimentation</h5>
                             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
@@ -64,8 +60,8 @@ get_header();
 
                 <div class="col-lg-4">
                     <div class="card" id="card3">
-                        <img src="http://marion2/wp-content/uploads/2019/09/pexels-photo-235990.jpg" class="card-img-top"  id="cardimg3"
-                            alt="...">
+                        <img src="http://marion2/wp-content/uploads/2019/09/pexels-photo-235990.jpg"
+                            class="card-img-top" id="cardimg3" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">Gestion du stress et des émotions</h5>
                             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
@@ -77,35 +73,30 @@ get_header();
                 </div>
             </div>
 
-            
+
 
 
 
         </div>
     </div>
 
-<!-- parallax -->
-
-<div img src= "http://marion2/wp-content/uploads/2019/09/pexels-photo-460621.jpeg" >
+    <!-- parallax -->
 
 
-            <div class="parallax"></div>
-            
-            <div style="height:1000px;background-color:rgba(133, 110, 110, 0.473);font-size:36px">
-                    Scroll Up and Down this page to see the parallax scrolling effect.
-                    This div is just here to enable scrolling.
-                    Tip: Try to remove the background-attachment property to remove the scrolling effect.
-            </div>
 
-            <div class="parallax"></div>
-        
-        </div>
-       
-            </div>
+</div>
 
 
 <!-- parallax fin -->
 
+<p>Après la naissance de ma première fille, je me suis tournée vers le naturel au quotidien. Il
+    était
+    primordial pour moi d’utiliser pour elle uniquement des produits naturels.
+    Le naturel c’est alors imposé partout : alimentation, santé, bien être, …
 
+    Pour en arriver là je me suis énormément documenté avec toujours l’envie d’en savoir plus. C’est
+    la
+    que la naturopathie est devenue une évidence pour moi.
+</p>
 
 </div>
